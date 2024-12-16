f5760735cd29b34c5b7a5d0f7a840ade570268ca
gzzz <405227329@qq.com>
2024-12-16 14:04:32 +0800

    light&dark

--- src/components/layouts/BaseHeader.vue
+++ src/components/layouts/BaseHeader.vue
@@ -39,8 +39,8 @@
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                width="24"
-                height="24"
+                width="20"
+                height="20"
               >
                 <path
                   fill-rule="evenodd"
